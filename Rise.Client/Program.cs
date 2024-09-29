d16c5c70df9278549ebe150de1bb12ce94b899f5
Benjamin Vertonghen <benjamin.vertonghen@append.be>
2024-09-29 10:27:20 +0200

    Defaults - With Swagger and Blazor Serving

--- Rise.Client/Program.cs
+++ Rise.Client/Program.cs
@@ -1,8 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.Web;
+using Rise.Client;
 using Rise.Client.Products;
 using Rise.Shared.Products;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
--- Rise.Server/Program.cs
+++ Rise.Server/Program.cs
@@ -6,9 +6,10 @@ using Rise.Shared.Products;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
-builder.Services.AddRazorComponents().AddInteractiveWebAssemblyComponents();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
@@ -24,24 +25,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
-
-app.UseRouting();
-app.UseAntiforgery();
-
-app.MapRazorPages();
 app.MapControllers();
-app.MapRazorComponents<Rise.Client.Host>().AddInteractiveWebAssemblyRenderMode();
+app.MapFallbackToFile("index.html");
 
 using (var scope = app.Services.CreateScope())
 { // Require a DbContext from the service provider and seed the database.
